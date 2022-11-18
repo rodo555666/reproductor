f74fa7c33c0bb54a67c907584c15f1a43b8be22b
rodolfoAcabrera <rodo555666@gmail.com>
2022-11-18 16:43:23 -0300

    ds

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const url = 'https://pokeapi.co/api/v2/pokemon/ditto'
+  const [todos, settodos] = useState()
+  const fetchpi = async () => {
+    const respon = await fetch(url)
+ 
+ const js = respon.json()
+ settodos(js)
+ console.log(todos)
+  }
 
+  useEffect(() => {
+    fetchpi()
+   }, [])
   return (
     <div className="App">
     </div>
--- src/canciones.jsx
+++ src/canciones.jsx
@@ -27,14 +27,38 @@ function canciones() {
     const [ t, sett ] = useState(0);
     const [ pimg, setpimg ] = useState(<i className="fa-solid fa-play"></i>);
  
+    const play = () => { setpl(true)
+        setpimg (<i className="fa-solid fa-play"></i>)
+       if (pl == false) {
+        sett(t+1)
+        setpimg ( <i className="fa-solid fa-pause"></i>)
+        
+         c[state].play()} else { setpl(false)
+            sett(0) 
+         c[state].pause() }
+        
+    };
+
+    const ante = ()=> { setState(state - 1)
+        setpl(false)
+       c[state].pause()
+       c[state].currentTime = 0
+      if (state <= 0 ) { setState(2) }};
+
+
+    const sig = () => { setState(state + 1)
+        c[state].pause()
+        setpl(false)
+        c[state].currentTime = 0
+      
+        if (state >= 2) { setState(0) } }
+
    /* useEffect(() => {
      if (pl == true) {
         sett(t+1) 
          minutes = Math.floor(t / 60)
      }
-    
       return () => {
-      
       }
     }, [t])*/
        
@@ -50,49 +74,23 @@ function canciones() {
             <h3>{datos[state].artista}</h3>
             <h2>{datos[state].cancion}</h2>
             <h4>{datos[state].disco}</h4>
+            <button onClick={play} className="play"> <div>{pimg}</div></button>
+            <div className='btn'>
             <button className='estrella'> <i className="fa-solid fa-star"></i></button> 
             <button className='aleatorio'><i className="fa-solid fa-shuffle"></i></button>
             <button className='repetir'><i className="fa-solid fa-repeat"></i></button>    
             <audio> <source src={datos[state].sound} type="audio/mp3" />  </audio>
-            <div className='tiempo'>  <p>{minutes} </p> </div>
-
-            <button onClick={()=> { setpl(true)
-            setpimg (<i className="fa-solid fa-play"></i>)
-           if (pl == false) {
-            sett(t+1)
-            setpimg ( <i className="fa-solid fa-pause"></i>)
-            
-             c[state].play()} else { setpl(false)
-                sett(0) 
-               
-             c[state].pause()  }
-            
-            }} className="play"> <div>{pimg}</div></button>
-
-
-
-            <button onClick={ () => { setState(state + 1)
-            c[state].pause()
-            setpl(false)
-            c[state].currentTime = 0
           
-            if (state >= 2) { setState(0) } }} className="siguiente"><img src="./svg/bxs-right-arrow.svg" alt="play" /></button>
 
-
-            <button onClick={()=> { setState(state - 1)
-             setpl(false)
-            c[state].pause()
-            c[state].currentTime = 0
-           /*setTimeout(() => {
-            c[state].play()
-           }, 200);*/
-
-           if (state <= 0 ) { setState(2) }}} className="anterior"><img src="./svg/bxs-left-arrow.svg" alt="play" /></button>
+           
+            <button onClick={sig} className="siguiente"><img src="./svg/bxs-right-arrow.svg" alt="play" /></button>
+            <button onClick={ante} className="anterior"><img src="./svg/bxs-left-arrow.svg" alt="play" /></button></div>
+              <div className='tiempo'>  <p>{minutes} </p> </div>
 
         </article>
        </div>
 
     );
-}
-
+        
+ }
 export default canciones
